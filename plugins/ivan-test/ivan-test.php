beabe02aea0bbde89fbbe5380a49b472c759b506
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2025-06-03 13:38:28 +0200

    Initial commit

---
+++ plugins/ivan-test/ivan-test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: Example plugin
+ * Description: Displays a simple admin notice in the WordPress admin dashboard.
+ * Version: 1.0
+ * Author: Ivan Ottinger
+ */
+
+// Hook into the 'admin_notices' action
+add_action( 'admin_notices', 'ane_show_admin_notice' );
+
+function ane_show_admin_notice() {
+    ?>
+    <div class="notice notice-success is-dismissible">
+        <p><strong>Admin Notice Example:</strong> This is a success message in the WordPress admin.</p>
+    </div>
+    <?php
+}
